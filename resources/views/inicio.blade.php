fa97cbdbb3b6081206408e74ac704a6be15532c3
Rodrigo Zavala Zevallos <rodizz@hotmail.com>
2017-02-24 14:59:15 +0100

    BD

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,15 +3,12 @@
     Disfrutar del Cine, cuida el mundo.
     @stop
 @section('texto')
-    <div id="carousel-example-generic" class="carousel slide " data-ride="carousel">
-
+    <div id="slider" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+            <li data-target="#slider" data-slide-to="0" class="active"></li>
+            <li data-target="#slider" data-slide-to="1"></li>
+            <li data-target="#slider" data-slide-to="2"></li>
         </ol>
-
         <div class="carousel-inner" role="listbox">
             <div class="item active">
                 <img src="Media/Slider/Imagen1.jpg" alt="...">
@@ -23,11 +20,11 @@
                 <img src="Media/Slider/Imagen4.jpg" alt="...">
             </div>
         </div>
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <a class="left carousel-control" href="#slider" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <a class="right carousel-control" href="#slider" role="button" data-slide="next">
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
